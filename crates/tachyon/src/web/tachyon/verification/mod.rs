45b50d313a8ae638d604b34ba0b814987bece4a8
aeoncl <c.lopez.dev@protonmail.com>
2026-04-13 00:24:19 +0200

    feat: emoji comparision page

--- crates/tachyon/src/web/tachyon/verification/mod.rs
+++ crates/tachyon/src/web/tachyon/verification/mod.rs
@@ -1,21 +1,20 @@
 pub(super) mod sas_v1_actions;
 pub(super) mod sas_v1;
 
+use crate::tachyon::alert::{AlertNotify, AlertSuccess};
 use crate::tachyon::global_state::GlobalState;
 use crate::tachyon::repository::RepositoryStr;
 use crate::web::tachyon::Params;
-use axum::extract::State;
-use axum::response::{Html, IntoResponse};
-use matrix_sdk::encryption::verification::VerificationRequestState;
-use matrix_sdk::ruma::OwnedUserId;
-use maud::{html, Markup};
-use std::str::FromStr;
 use anyhow::anyhow;
-use axum::http::{Response, StatusCode};
-use axum::http::header::CONTENT_TYPE;
+use axum::extract::State;
+use axum::response::IntoResponse;
+use matrix_sdk::encryption::verification::VerificationRequestState;
 use matrix_sdk::ruma::events::key::verification::VerificationMethod;
-use mime::TEXT_HTML;
-use crate::tachyon::alert::{AlertNotify, AlertSuccess};
+use matrix_sdk::ruma::OwnedUserId;
+use maud::html;
+use std::str::FromStr;
+use axum::body::Body;
+use axum::http::Response;
 
 pub async fn get_verification_poll(
     State(state): State<GlobalState>,
@@ -30,6 +29,9 @@ pub async fn get_verification_poll(
     let user_id_raw = params.get("user_id").map(|s| s.as_str()).unwrap_or_default();
     let user_id = OwnedUserId::from_str(user_id_raw).unwrap();
 
+    let last_state = params.get("state").map(|s| s.as_str()).unwrap_or_default().trim();
+
+
     let tachyon_client = state.tachyon_clients().get(&token).unwrap();
     if !tachyon_client.alerts().contains_key(&notification_id)
     {
@@ -40,12 +42,21 @@ pub async fn get_verification_poll(
 
     let verification_request = state.pending_verification_requests().get(&flow_id).unwrap();
 
+    if last_state != "req_transitioned" && last_state == state_name(&verification_request.state()) {
+        return Response::builder()
+            .status(204)
+            .body(Body::empty())
+            .unwrap();
+    }
+
+
     let refresh_url = format!("/tachyon/verification?notification_id={}&flow_id={}&user_id={}", notification_id, flow_id, user_id_raw);
+    let refresh_url_with_state = format!("{}&state={}", refresh_url, state_name(&verification_request.state()));
 
     let response = match verification_request.state() {
         VerificationRequestState::Created { our_methods } => {
              html! {
-                    div class="container" ic-poll="1s" ic-src=(refresh_url) ic-replace-target="true" {
+                    div class="container" ic-poll="1s" ic-src=(refresh_url_with_state) ic-replace-target="true" {
                         table class="hero-table" cellspacing="0" cellpadding="0" border="0" {
                             tr {
                                 td class="hero-text" valign="middle" {
@@ -59,7 +70,7 @@ pub async fn get_verification_poll(
         }
         VerificationRequestState::Requested { their_methods, other_device_data } => {
             html! {
-                    div class="container" ic-poll="1s" ic-src=(refresh_url) ic-replace-target="true" {
+                    div class="container" ic-poll="1s" ic-src=(refresh_url_with_state) ic-replace-target="true" {
                         table class="hero-table" cellspacing="0" cellpadding="0" border="0" {
                             tr {
                                 td class="hero-text" valign="middle" {
@@ -80,7 +91,7 @@ pub async fn get_verification_poll(
             }
 
             html! {
-                    div class="container" ic-poll="1s" ic-src=(refresh_url) ic-replace-target="true" {
+                    div class="container" ic-poll="1s" ic-src=(refresh_url_with_state) ic-replace-target="true" {
                         table class="hero-table" cellspacing="0" cellpadding="0" border="0" {
                             tr {
                                 td class="hero-text" valign="middle" {
@@ -93,8 +104,9 @@ pub async fn get_verification_poll(
 
         }
         VerificationRequestState::Transitioned { verification } => {
+
             if let Some(verification) = verification.sas() {
-                sas_v1::handle_sas_v1(verification, &refresh_url, notification_id, flow_id, &user_id).await
+                return sas_v1::handle_sas_v1(verification, &last_state, &refresh_url, notification_id, flow_id, &user_id).await
             } else {
                 html! {
                     div class="container" {
@@ -147,6 +159,16 @@ pub async fn get_verification_poll(
         }
     };
 
-    response
+    response.into_response()
 }
 
+fn state_name(state: &VerificationRequestState) -> &'static str {
+    match state {
+        VerificationRequestState::Created { .. } => "req_created",
+        VerificationRequestState::Requested { .. } => "req_requested",
+        VerificationRequestState::Ready { .. } => "req_ready",
+        VerificationRequestState::Transitioned { .. } => "req_transitioned",
+        VerificationRequestState::Done => "req_done",
+        VerificationRequestState::Cancelled(_) => "req_cancelled",
+    }
+}
--- crates/tachyon/src/web/tachyon/verification/sas_v1.rs
+++ crates/tachyon/src/web/tachyon/verification/sas_v1.rs
@@ -1,20 +1,35 @@
-use matrix_sdk::encryption::verification::{SasState, SasVerification};
+use axum::body::Body;
+use axum::http::Response;
+use axum::response::IntoResponse;
+use matrix_sdk::encryption::verification::{Emoji, SasState, SasVerification};
 use matrix_sdk::ruma::UserId;
 use maud::{html, Markup};
 
 pub async fn handle_sas_v1(
     verification: SasVerification,
+    last_state: &str,
     refresh_url: &str,
     notification_id: i32,
     flow_id: &str,
     user_id: &UserId
-) -> Markup {
+) -> Response<Body> {
+
+    let state_name = state_name(&verification.state());
+
+    if last_state  == state_name {
+        return Response::builder()
+            .status(204)
+            .body(Body::empty())
+            .unwrap();
+    }
+
+    let refresh_url_with_state = format!("{}&state={}", refresh_url, state_name);
 
 
     let response = match verification.state() {
             SasState::Created { protocols } => {
                 html! {
-                    div class="container" ic-poll="1s" ic-src=(refresh_url) ic-replace-target="true" {
+                    div class="container" ic-poll="1s" ic-src=(refresh_url_with_state) ic-replace-target="true" {
                         table class="hero-table" cellspacing="0" cellpadding="0" border="0" {
                             tr {
                                 td class="hero-text" valign="middle" {
@@ -30,7 +45,7 @@ pub async fn handle_sas_v1(
                 verification.accept().await.unwrap();
 
                 html! {
-                    div class="container" ic-poll="1s"  ic-src=(refresh_url) ic-replace-target="true" {
+                    div class="container" ic-poll="1s"  ic-src=(refresh_url_with_state) ic-replace-target="true" {
                         table class="hero-table" cellspacing="0" cellpadding="0" border="0" {
                             tr {
                                 td class="hero-text" valign="middle" {
@@ -43,7 +58,7 @@ pub async fn handle_sas_v1(
             }
             SasState::Accepted { accepted_protocols } => {
                 html! {
-                    div class="container" ic-poll="1s" ic-src=(refresh_url) ic-replace-target="true" {
+                    div class="container" ic-poll="1s" ic-src=(refresh_url_with_state) ic-replace-target="true" {
                         table class="hero-table" cellspacing="0" cellpadding="0" border="0" {
                             tr {
                                 td class="hero-text" valign="middle" {
@@ -59,44 +74,55 @@ pub async fn handle_sas_v1(
 
 
                 let emoji_str = emojis.unwrap();
-
-                let emoji_array = emoji_str.emojis;
+                let emojis = emoji_str.emojis;
 
                 html! {
-                    div class="container" ic-src=(refresh_url) ic-replace-target="true" {
+                    div class="container" ic-poll="2s" ic-src=(refresh_url_with_state) ic-replace-target="true" {
                         table class="hero-table" cellspacing="0" cellpadding="0" border="0" {
                             tr {
                                 td class="hero-text" valign="middle" {
-                                    h2 { "Compare these emojis on both your devices" }
-                                    p { "Please wait while devices communicate." }
+                                    h2 { "Compare the emojis" }
+                                    p {
+                                        b{ "Check" }
+                                        " if the "
+                                        b{ "emojis" }
+                                        " showed here "
+                                        b { "match" }
+                                        " with the ones on the other device."
+                                        br{
+                                            "If they do, "
+                                            i { "you're all good." }
+                                            " (H)"
+                                        }
+                                    }
                                 }
                             }
                         }
 
+                        (emoji_table(&emojis))
 
-                        @for emoji in emoji_array {
-                            span { (emoji.symbol) }
-                        }
+                        div class="spacer" {}
 
-                           form ic-post-to="/tachyon/verification/sas_v1/confirm" ic-target="closest div.container" {
+                           form class="single-btn-form" ic-post-to="/tachyon/verification/sas_v1/confirm" ic-target="closest div.container" {
                                 input type="hidden" name="notification_id" value=(notification_id);
                                 input type="hidden" name="flow_id" value=(flow_id);
                                 input type="hidden" name="user_id" value=(user_id);
 
                                 button type="submit" class="btn btn-primary" {
                                     span class="btn-shine" {}
-                                    span class="btn-text" { "The emojis match !" }
+                                    span class="btn-label" { "They match !" }
                                 }
                             }
 
-                            form ic-post-to="/tachyon/verification/sas_v1/mismatch" ic-target="closest div.container" {
+
+                            form class="single-btn-form back-btn-form" ic-post-to="/tachyon/verification/sas_v1/mismatch" ic-target="closest div.container" {
                                 input type="hidden" name="notification_id" value=(notification_id);
                                 input type="hidden" name="flow_id" value=(flow_id);
                                 input type="hidden" name="user_id" value=(user_id);
 
                                 button type="submit" class="btn btn-danger" {
                                     span class="btn-shine" {}
-                                    span class="btn-text" { "The emojis don't match !" }
+                                    span class="btn-label" { "They don't match" }
                                 }
                             }
 
@@ -105,7 +131,7 @@ pub async fn handle_sas_v1(
             }
             SasState::Confirmed => {
                 html! {
-                    div class="container" ic-poll="1s" ic-src=(refresh_url) ic-replace-target="true" {
+                    div class="container" ic-poll="1s" ic-src=(refresh_url_with_state) ic-replace-target="true" {
                         table class="hero-table" cellspacing="0" cellpadding="0" border="0" {
                             tr {
                                 td class="hero-text" valign="middle" {
@@ -147,5 +173,53 @@ pub async fn handle_sas_v1(
             }
         };
 
-    response
+    response.into_response()
+}
+
+fn emoji_table(emojis: &[Emoji]) -> Markup {
+
+    let (first_emoji_row, second_emoji_row) = emojis.split_at(4);
+
+    html! {
+        div class="emoji-container" {
+            table class="emoji-table" cellspacing="0" cellpadding="0" border="0" {
+                tr {
+                    @for emoji in first_emoji_row {
+                        ( emoji_to_html(&emoji) )
+                    }
+                }
+            }
+            div class="spacer" {}
+            table class="emoji-table" cellspacing="0" cellpadding="0" border="0" {
+                tr {
+                    @for emoji in second_emoji_row {
+                        ( emoji_to_html(&emoji) )
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+fn state_name(state: &SasState) -> &'static str {
+    match state {
+        SasState::Created { .. } => "sas_created",
+        SasState::Started { .. } => "sas_started",
+        SasState::Accepted { .. } => "sas_accepted",
+        SasState::KeysExchanged { .. } => "sas_keys_exchanged",
+        SasState::Confirmed => "sas_confirmed",
+        SasState::Done { .. } => "sas_done",
+        SasState::Cancelled(_) => "sas_cancelled",
+    }
+}
+
+fn emoji_to_html(emoji: &matrix_sdk::encryption::verification::Emoji) -> Markup {
+    let img_url = format!("img/sas_v1/{}.gif", emoji.description.to_lowercase().replace(" ", "_"));
+    html! {
+        td {
+            img src=(img_url) alt=(emoji.description) {}
+            p { (emoji.description) }
+        }
+    }
 }
